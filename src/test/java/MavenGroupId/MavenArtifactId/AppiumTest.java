a0b4e848b485c0db59926091abab124f2022d489
kritika <kritikakesarwani15@gmail.com>
2022-02-03 15:42:06 +0530

    first commit

---
+++ src/test/java/MavenGroupId/MavenArtifactId/AppiumTest.java
@@ -0,0 +1,19 @@
+package MavenGroupId.MavenArtifactId;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.Test;
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+	
+	@Test
+	public void AppiumTest1() {
+		System.out.println("AppiumTest1");
+	}
+	
+	@Test
+	public void AppiumTest2() {
+		System.out.println("AppiumTest2");
+	}
+
+}
---
+++ src/test/java/MavenGroupId/MavenArtifactId/RESTAPITest.java
@@ -0,0 +1,19 @@
+package MavenGroupId.MavenArtifactId;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.Test;
+import org.testng.annotations.Test;
+
+public class RESTAPITest {
+	
+	@Test
+	public void RestApiTest1() {
+		System.out.println("RestApiTest1");
+	}
+	
+	@Test
+	public void RestApiTest2() {
+		System.out.println("RestApiTest2");
+	}
+
+}
