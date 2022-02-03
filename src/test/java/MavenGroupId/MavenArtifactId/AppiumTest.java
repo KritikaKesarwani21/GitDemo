d2a9a87262f7608f4600311e86c6c778405e2f94
kritika <kritikakesarwani15@gmail.com>
2022-02-03 22:08:14 +0530

    git demo develop

--- src/test/java/MavenGroupId/MavenArtifactId/AppiumTest.java
+++ src/test/java/MavenGroupId/MavenArtifactId/AppiumTest.java
@@ -9,11 +9,21 @@ public class AppiumTest {
 	@Test
 	public void AppiumTest1() {
 		System.out.println("AppiumTest1");
+		System.out.println("AppiumTest1 gitdemo develop");
+		System.out.println("AppiumTest1 gitdemo develop1");
 	}
 	
 	@Test
 	public void AppiumTest2() {
 		System.out.println("AppiumTest2");
+		System.out.println("AppiumTest2 gitdemo develop");
+		System.out.println("AppiumTest2 gitdemo develop 1");
+	}
+	@Test
+	public void AppiumTest3() {
+		System.out.println("AppiumTest3");
+		System.out.println("AppiumTest3 gitdemo develop");
+		System.out.println("AppiumTest3 gitdemo develop1");
 	}
 
 }
